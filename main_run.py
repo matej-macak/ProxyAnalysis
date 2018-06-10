0014d7a234ad7b66fa07492af05a4f748666ac04
root <root@ip-172-31-23-130.us-west-2.compute.internal>
2018-06-10 15:01:44 +0000

    Added additional parse argument for number of tweets.

--- main_run.py
+++ main_run.py
@@ -7,16 +7,18 @@ from bs4 import BeautifulSoup
 import tweepy
 from tweepy import StreamListener
 import ast
+import time
   
 
 class MyStreamListener(StreamListener):
     
-    def __init__(self, output_path):
+    def __init__(self, output_path, no_per_file):
         StreamListener.__init__(self)
         
         # Initialise counter and storage variables
         self._counter = 1
         self._output_path = output_path
+        self._no_per_file = no_per_file
         self._data = []
 
     def on_connect(self):
@@ -28,13 +30,15 @@ class MyStreamListener(StreamListener):
         print('An Error has occured: ' + repr(status_code))
         return False    
         
-    def on_status(self, status, no_per_file = 1000):
+    def on_status(self, status):
         """ Main method to output data into files from the Twitter stream. Number of statuses are defined by
         no_per_file attribute."""
 
         self._data.append(status._json)
-        if self._counter % no_per_file == 0:
-            file_name = "{}/{}.json".format(self._output_path,self._counter)
+        if self._counter % self._no_per_file == 0:
+            # Get current date and output JSON
+            date = time.strftime('%Y-%m-%d_%H-%M-%S', time.gmtime(time.time()))
+            file_name = "{}/{}.json".format(self._output_path,date)
             with open(file_name, 'w') as outfile:
                 json.dump(self._data, outfile)
         
@@ -51,12 +55,12 @@ if __name__ == "__main__":
 
     parser.add_argument("output", help = "Output path for twitter data.")
     parser.add_argument("credentials", help = "Path to twitter development credentials. Expects JSON file.")
-    
-    print(parser.parse_args().credentials)
-    
+    parser.add_argument("--no_tweets", dest = "no_tweets", help="Save every N number of tweets.", default=1000, type=int )
+        
     credentials_path = parser.parse_args().credentials
     output_path = parser.parse_args().output
-    
+    no_tweets = parser.parse_args().no_tweets
+        
     with open(credentials_path,"r") as fp:
         credentials = json.load(fp)
     
@@ -64,8 +68,8 @@ if __name__ == "__main__":
     auth.set_access_token(credentials["ACCESS_TOKEN"], credentials["ACCESS_SECRET"])
     api = tweepy.API(auth,wait_on_rate_limit=True)
 
-    myStreamListener = MyStreamListener(output_path)
-    myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener(output_path))
+    myStreamListener = MyStreamListener(output_path, no_tweets)
+    myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener(output_path, no_tweets))
 
     syria = (35.7270, 32.3106, 42.3850, 37.3190)
     myStream.filter(locations=syria)
